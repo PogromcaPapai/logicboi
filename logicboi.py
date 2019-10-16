19568e4926d0e9e093b1301915327f20f6f869be
Jakub Dakowski <jakubdakowski@gmail.com>
2019-10-16 12:20:36 +0200

    Added implication; implemented tests for all Sentence subclasses

--- logicboi.py
+++ logicboi.py
@@ -4,11 +4,20 @@ class Sentence(object):
     truth_table = dict()
     arg_number = 0
 
-    def __init__(self, args, string='', value=None):
+    def __init__(self, string, value, *args):
+        '''
+        Creates a representation of a sentence
+
+        Args:
+         - `string` - `string` - used as a character representation of the sentence, ex. `p, q, p^q`  
+         - `value` - `True/False/None` - used to provide an input for sentence's logical value (None if unknown)  
+         - `*args` - `Sentence` - used to input arguments of the sentence
+        '''
         super().__init__()
         self.string = string
         self.value = value
-        assert self.arg_number == len(args), "This amount of arguments is not allowed here: {0}".format(self.__str__())
+        if not self.arg_number == len(args):
+            raise TypeError("This amount of arguments is not allowed here: {0}".format(self.__str__()))
         self.args = args
 
     def __repr__(self):
@@ -18,6 +27,7 @@ class Sentence(object):
         return self.string
 
     def evaluate(self):
+        ''' Evaluates the value of the sentence '''
         if self.value != None:
             pass
         elif self.truth_table == None:
@@ -30,8 +40,8 @@ class Sentence(object):
 ### Functor definitions 
 ######
 
-Verum = Sentence([], string="T", value=True)
-Falsum = Sentence([], string="F", value=False)
+Verum = Sentence("T", True)
+Falsum = Sentence("F", False)
 
 class Negation(Sentence):
     truth_table = {True:False, False:True}
@@ -45,8 +55,12 @@ class Alternative(Sentence):
     truth_table = {True:{True:True, False:True}, False:{True:True, False:False}}
     arg_number = 2
 
+class Implication(Sentence):
+    truth_table = {True:{False:False, True:True}, False:{False:True, True:True}}
+    arg_number = 2
+
 if __name__ == "__main__":
-    test1 = Sentence([], value=True)
-    test2 = Sentence([], value=True)
-    tested = Conjunction([test1, test2])
+    test1 = Sentence('', True)
+    test2 = Sentence('', True)
+    tested = Conjunction('', None, test1, test2)
     print(tested.evaluate())
--- logicboi_test.py
+++ logicboi_test.py
@@ -1,31 +1,77 @@
 import logicboi
 import pytest
 
+def basetest(model_class, tested_function, *values):
+    sentences = []
+    for i in values:
+        sentences.append(logicboi.Sentence(str(i), i))
+    correct_val = tested_function(*values)
+    tested = model_class('', None, *sentences)
+    assert tested.evaluate() == correct_val
+    
 
 class TestNegation:
 
     def test_true(self):
-        test = logicboi.Sentence([], value=True)
-        tested = logicboi.Negation([test])
-        assert tested.evaluate() == False
+        basetest(logicboi.Negation, lambda x: not x, True)
 
     def test_false(self):
-        test = logicboi.Sentence([], value=False)
-        tested = logicboi.Negation([test])
-        assert tested.evaluate() == True
+        basetest(logicboi.Negation, lambda x: not x, False)
 
     def test_additional(self):
-        with pytest.raises(AssertionError):
-            logicboi.Negation([logicboi.Sentence([]), logicboi.Sentence([])])
+        with pytest.raises(TypeError):
+            logicboi.Negation(logicboi.Sentence(), logicboi.Sentence())
 
 class TestConjunction:
 
     def test_truextrue(self):
-        test1 = logicboi.Sentence([], value=True)
-        test2 = logicboi.Sentence([], value=True)
-        tested = logicboi.Conjunction([test1, test2])
-        assert tested.evaluate() == True
+        basetest(logicboi.Conjunction, lambda x,y: x and y, True, True)
+
+    def test_falsextrue(self):
+        basetest(logicboi.Conjunction, lambda x,y: x and y, False, True)
+
+    def test_truexfalse(self):
+        basetest(logicboi.Conjunction, lambda x,y: x and y, True, False)
+
+    def test_falsexfalse(self):
+        basetest(logicboi.Conjunction, lambda x,y: x and y, False, False)
 
     def test_additional(self):
-        with pytest.raises(AssertionError):
-            logicboi.Negation([logicboi.Sentence([]), logicboi.Sentence([]), logicboi.Sentence([])])
+        with pytest.raises(TypeError):
+            logicboi.Conjunction('', None, logicboi.Sentence('', None), logicboi.Sentence('', None), logicboi.Sentence('', None))
+
+class TestAlternative:
+
+    def test_truextrue(self):
+        basetest(logicboi.Alternative, lambda x,y: x or y, True, True)
+
+    def test_falsextrue(self):
+        basetest(logicboi.Alternative, lambda x,y: x or y, False, True)
+
+    def test_truexfalse(self):
+        basetest(logicboi.Alternative, lambda x,y: x or y, True, False)
+
+    def test_falsexfalse(self):
+        basetest(logicboi.Alternative, lambda x,y: x or y, False, False)
+
+    def test_additional(self):
+        with pytest.raises(TypeError):
+            logicboi.Alternative('', None, logicboi.Sentence('', None), logicboi.Sentence('', None), logicboi.Sentence('', None))
+
+class TestImplication:
+
+    def test_truextrue(self):
+        basetest(logicboi.Implication, lambda x,y: (not x) or y, True, True)
+
+    def test_falsextrue(self):
+        basetest(logicboi.Implication, lambda x,y: (not x) or y, False, True)
+
+    def test_truexfalse(self):
+        basetest(logicboi.Implication, lambda x,y: (not x) or y, True, False)
+
+    def test_falsexfalse(self):
+        basetest(logicboi.Implication, lambda x,y: (not x) or y, False, False)
+
+    def test_additional(self):
+        with pytest.raises(TypeError):
+            logicboi.Implication('', None, logicboi.Sentence('', None), logicboi.Sentence('', None), logicboi.Sentence('', None))
